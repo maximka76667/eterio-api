8871cd3893f2493bf43a119515c08631c3d3f441
Maxim <74974283+maximka76667@users.noreply.github.com>
2023-04-26 11:21:28 +0200

    add cors

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pydantic import BaseModel
 from google.cloud import firestore
 import os
 from firebase_admin import credentials, firestore, initialize_app
+from fastapi.middleware.cors import CORSMiddleware
 
 from users.routes import router as users_router
 from drinks.routes import router as drinks_router
@@ -17,6 +18,13 @@ cred = credentials.Certificate(
 initialize_app(cred)
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users_router, prefix="/users", tags=["users"])
 app.include_router(drinks_router, prefix="/drinks", tags=["drinks"])
